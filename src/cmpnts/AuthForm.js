72f934b91fe318f97381b23c92c5b2cda68f41c0
zatorriusf <zatorrius.furniss@outlook.com>
2020-10-14 09:58:46 -0400

    Added spacing for buttons

--- src/cmpnts/AuthForm.js
+++ src/cmpnts/AuthForm.js
@@ -1,38 +1,47 @@
-import React,{useRef} from "react";
-import { Button, Form } from "react-bootstrap";
+import React, { useRef } from "react";
+import { Button, Form, Row } from "react-bootstrap";
 
-const AuthForm = ({ handleModal, handleAuth,confirmText }) => {
+const AuthForm = ({ handleModal, handleAuth, confirmText }) => {
   const emailRef = useRef();
   const passwordRef = useRef();
   const submitAuthInfo = () => {
-    if(emailRef.current.value === '' || passwordRef.current.value === '' ){
+    if (emailRef.current.value === "" || passwordRef.current.value === "") {
       return;
     }
     const authObject = {
-      email : emailRef.current.value,
-      password : passwordRef.current.value
-    }
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    };
     handleAuth(authObject);
     handleModal();
   };
   return (
-    <Form className="p-3">
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" ref={emailRef}/>
-      </Form.Group>
+    <>
+      <Form className="p-3">
+        <h3>{confirmText}</h3>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" ref={emailRef} />
+        </Form.Group>
 
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" ref={passwordRef}/>
-      </Form.Group>
-      <Button variant="secondary" onClick={() => handleModal()}>
-        Cancel
-      </Button>
-      <Button variant="info" onClick={submitAuthInfo}>
-        {confirmText}
-      </Button>
-    </Form>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            ref={passwordRef}
+          />
+        </Form.Group>
+        <Row className="d-flex justify-content-between p-3">
+          <Button variant="secondary" onClick={() => handleModal()}>
+            Cancel
+          </Button>
+          <Button variant="info" onClick={submitAuthInfo}>
+            {confirmText}
+          </Button>
+        </Row>
+      </Form>
+    </>
   );
 };
 
