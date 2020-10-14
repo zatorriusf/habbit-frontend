1cfadf06422da43bd426432e4392993f9ff610bf
zatorriusf <zatorrius.furniss@outlook.com>
2020-10-14 10:21:29 -0400

    added additional styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,14 @@
 import React from "react";
-import {  Jumbotron } from "react-bootstrap";
+import { Jumbotron } from "react-bootstrap";
 
 const Home = () => {
   return (
     <Jumbotron>
       <h2>habbitual</h2>
-      <h3>a habit tracker for coding practice</h3>
+      <h3 className="font-weight-light">a habit tracker for coding practice</h3>
       <p>
-        This habit tracker was created to pracitce working in 
-        in a MERN Stack. The front end stying is done with 
-        react-bootstrap components.
+        This habit tracker was created to pracitce working in in a MERN Stack.
+        The front end stying is done with react-bootstrap components.
       </p>
     </Jumbotron>
   );
